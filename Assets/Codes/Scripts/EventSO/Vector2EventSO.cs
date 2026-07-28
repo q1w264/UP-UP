c37a31bbb32072b0b8a2c4ec1c4ca379636da154
Shilong Yuan <shilong.yuan@q1w264.com>
2026-07-28 17:41:37 +0800

    feat(EventSO): add Vector2EventSO

---
+++ Assets/Codes/Scripts/EventSO/Vector2EventSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Codes.Scripts.EventSO
+{
+    [CreateAssetMenu(fileName = "New Vector2 Event SO", menuName = "SOEvents/Vector2EventSO")]
+    public class Vector2EventSO : BaseEventSO<Vector2>
+    {
+    }
+}
